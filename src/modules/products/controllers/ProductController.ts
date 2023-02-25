6d4d503147ad605f668edb55d5873a3df7ac328b
Marcio Carolino <marciocarolino@gmail.com>
2023-02-25 14:51:35 -0300

    testando o git

--- src/modules/products/controllers/ProductController.ts
+++ src/modules/products/controllers/ProductController.ts
@@ -1,3 +1,11 @@
-class ProductController {}
+import { get } from 'http';
+import Product from '../typeorm/entities/Product';
+
+class ProductController {
+
+  async productAll(): Promise<Product> {
+    const product = await
+  }
+}
 
 export default ProductController;
