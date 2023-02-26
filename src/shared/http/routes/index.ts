726de6efd580b778d8d7a4d9095e95cefbfdf83a
Marcio Carolino <marciocarolino@gmail.com>
2023-02-26 17:41:18 -0300

    limpando o código

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,11 +3,6 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.send({ message: 'Hello Dev!' });
-});
-
 routes.use('/products', productsRouter);
-
 
 export default routes;
