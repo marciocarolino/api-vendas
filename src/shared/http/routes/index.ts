9f8a1f13bf72bd4656a49e891d68b70433fc3d53
Marcio Carolino <marciocarolino@gmail.com>
2023-02-26 17:40:54 -0300

    feito a rota para products

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -5,5 +6,8 @@ const routes = Router();
 routes.get('/', (request, response) => {
   return response.send({ message: 'Hello Dev!' });
 });
+
+routes.use('/products', productsRouter);
+
 
 export default routes;
